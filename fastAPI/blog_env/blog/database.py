f1452dc16f2bc235c293189df091eebc4a7a3b8f
Mukama <mk.frmh3@gmail.com>
2024-04-03 11:48:18 +0200

    deleted submodile

--- fastAPI/blog_env/blog/database.py
+++ fastAPI/blog_env/blog/database.py
@@ -1,14 +1,18 @@
 #database.py
 from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import psycopg2
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
-
-
-engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
