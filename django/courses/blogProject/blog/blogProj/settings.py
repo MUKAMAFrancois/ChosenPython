eeb1b8497f303a998464fb64289abe50fbad6b8c
Mukama <mk.frmh3@gmail.com>
2024-04-14 11:16:06 +0200

    py manage.py collectstatic

--- django/courses/blogProject/blog/blogProj/settings.py
+++ django/courses/blogProject/blog/blogProj/settings.py
@@ -162,6 +162,7 @@ EMAIL_PORT = '2525'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=(BASE_DIR/'static',)
+STATIC_ROOT = os.path.join(BASE_DIR,"assets")
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
