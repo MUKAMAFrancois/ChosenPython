e4cf9e12f5b4f498eaf5297d9c6d71a9198b1e20
Mukama <mk.frmh3@gmail.com>
2024-04-19 15:13:46 +0200

    crud serializers

--- drf/drf1_env/DRF1/posts/urls.py
+++ drf/drf1_env/DRF1/posts/urls.py
@@ -4,15 +4,17 @@ import uuid
 
 urlpatterns = [
 
-    path('home',views.homepage,name='homepage'),
+    # path('home',views.homepage,name='homepage'),
     path('all',views.list_posts,name='listofposts'),
+    path('create',views.create_a_post,name='createpost'),
+    path('post/<uuid:post_id>',views.single_post,name='singlepost'),
+    path('post/<uuid:post_id>/update',views.update_post,name='updatepost'),
+    path('post/<uuid:post_id>/delete',views.delete_post,name='deletepost'),
 
-    path('post/<int:post_id>',views.single_post,name='singlepost'),
 
     # serializers
+    # path('',views.Post_ListCreate.as_view(),name='post_list'),
 
-    path('',views.Post_ListCreate.as_view(),name='post_list'),
-
-    path('<uuid:pk>',views.Post_RetrieveUpdateDestroy.as_view(),name='post_detail'),
+    # path('<uuid:pk>',views.Post_RetrieveUpdateDestroy.as_view(),name='post_detail'),
     
 ]
--- drf/drf1_env/DRF1/posts/views.py
+++ drf/drf1_env/DRF1/posts/views.py
@@ -1,5 +1,5 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -11,56 +11,107 @@ import uuid
 # Create your views here.
 
 
-posts=[{
-    "id":1,
-    "title":"Post 1",
-    "content":"This is the first post"
-},
-{
-    "id":2,
-    "title":"Post 2",
-    "content":"This is the second post"
-},
-{
-    "id":3,
-    "title":"Post 3",
-    "content":"This is the third post"
-}
+# posts=[{
+#     "id":1,
+#     "title":"Post 1",
+#     "content":"This is the first post"
+# },
+# {
+#     "id":2,
+#     "title":"Post 2",
+#     "content":"This is the second post"
+# },
+# {
+#     "id":3,
+#     "title":"Post 3",
+#     "content":"This is the third post"
+# }
 
-]
+# ]
 
-@api_view(http_method_names=["GET","POST"])
-def homepage(request:Request):
+# @api_view(http_method_names=["GET","POST"])
+# def homepage(request:Request):
 
-    if request.method == 'POST':
-        # data = json.loads(request.body.decode('utf-8'))
-        data=request.data #no manual decoding needed, request.data does it
-        res={
-            "msg":"successfully posted!",
-            "data":data
-        }
-        return Response(data=res,status=201)
+#     if request.method == 'POST':
+#         # data = json.loads(request.body.decode('utf-8'))
+#         data=request.data #no manual decoding needed, request.data does it
+#         res={
+#             "msg":"successfully posted!",
+#             "data":data
+#         }
+#         return Response(data=res,status=201)
 
-    response={
-        "message":"Welcome to the homepage"
-    }
-    return Response(data=response,status=200,headers={"Content-Type":"application/json"})
+#     response={
+#         "message":"Welcome to the homepage"
+#     }
+#     return Response(data=response,status=200,headers={"Content-Type":"application/json"})
 
 
 
 @api_view(http_method_names=["get"])
 def list_posts(request:Request):
-    return Response(data=posts,status=200)
+    posts=Post.objects.all()
+    serializer = PostSerializer(instance=posts,many=True)
+    return Response(data=serializer.data,status=200)
 
+@api_view(http_method_names=["POST"])
+def create_a_post(request:Request):
+    if request.method=="POST":
+        data=request.data
+        serializer=PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            res={
+                "msg":"Post created successfully",
+                "data":serializer.data
+            }
+            return Response(data=res,status=201)
+        return Response(data=serializer.errors,status=400)
 
-@api_view(http_method_names=["GET"])
-def single_post(request:Request,post_id:int):
-    post = posts[post_id]
-
+@api_view(http_method_names=["GET"])    
+def single_post(request,post_id:uuid.UUID):
+    post=get_object_or_404(Post,pk=post_id)
+    serializer=PostSerializer(instance=post)
     if post:
-        return Response(data=post,status=200)
-    
+        return Response(data=serializer.data,status=200)
     return Response(data={"message":"Post not found"},status=404)
+
+
+@api_view(http_method_names=["PATCH"])
+def update_post(request,post_id:uuid.UUID):
+    post=get_object_or_404(Post,pk=post_id)
+    serializer=PostSerializer(instance=post)
+    if request.method=="PATCH":
+        data=request.data
+        serializer=PostSerializer(instance=post,data=data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res={
+                "msg":"Post updated successfully",
+                "data":serializer.data
+            }
+            return Response(data=res,status=200)
+        return Response(data=serializer.errors,status=400)
+    return Response(data={"message":"Post not found"},status=404)
+
+@api_view(http_method_names=["DELETE"])
+def delete_post(request,post_id:uuid.UUID):
+    post=get_object_or_404(Post,pk=post_id)
+    if post:
+        post.delete()
+        return Response(data={"message":"Post deleted successfully"},status=200)
+    return Response(data={"message":"Post not found"},status=404)
+
+
+
+
+# @api_view(http_method_names=["GET"])
+# def single_post(request:Request,post_id:uuid.UUID):
+#     post =get_object_or_404(Post,pk=post_id)
+#     serializer=PostSerializer(post)
+#     if post:
+#         return Response(data=serializer.data,status=200)
+#     return Response(data={"message":"Post not found"},status=404)
 
 
 
@@ -68,46 +119,46 @@ def single_post(request:Request,post_id:int):
 # ##################
 
 
-class Post_ListCreate(APIView):
-    def get(self,request:Request):
-        posts=Post.objects.all()
-        serializer=PostSerializer(posts,many=True)
-        return Response(data=serializer.data,status=200)
+# class Post_ListCreate(APIView):
+#     def get(self,request:Request):
+#         posts=Post.objects.all()
+#         serializer=PostSerializer(posts,many=True)
+#         return Response(data=serializer.data,status=200)
     
 
-    def post(self, request: Request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request: Request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class Post_RetrieveUpdateDestroy(APIView):
-    def get(self,request:Request,pk:uuid.UUID):
-        try:
-            post=Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND,data={"message":"Post not found"})
-        serializer=PostSerializer(post)
-        return Response(data=serializer.data,status=200)
+# class Post_RetrieveUpdateDestroy(APIView):
+#     def get(self,request:Request,pk:uuid.UUID):
+#         try:
+#             post=Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND,data={"message":"Post not found"})
+#         serializer=PostSerializer(post)
+#         return Response(data=serializer.data,status=200)
     
-    def patch(self, request:Request, pk:uuid.UUID):
-        try:
-            post = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+#     def patch(self, request:Request, pk:uuid.UUID):
+#         try:
+#             post = Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = PostSerializer(post, data=request.data, partial=True) # partial=True allows for partial updates, not all fields are required
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         serializer = PostSerializer(post, data=request.data, partial=True) # partial=True allows for partial updates, not all fields are required
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self,request:Request,pk:uuid.UUID):
-        try:
-            post=Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND, data={"message":"Post not found"})
-        post.delete()
-        return Response(data={"message":"Post deleted successfully"},status=200)
+#     def delete(self,request:Request,pk:uuid.UUID):
+#         try:
+#             post=Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND, data={"message":"Post not found"})
+#         post.delete()
+#         return Response(data={"message":"Post deleted successfully"},status=200)
